37eba6e1e2ae5aaff27da52c0917ace7cb5cdb4c
Caitlin Bopp <boppcaitlin@gmail.com>
2021-05-09 20:01:02 -0400

    Added schema.sql and seeds.sql files

---
+++ db/schema.sql
@@ -0,0 +1,17 @@
+DROP DATABASE IF EXISTS employees
+CREATE DATABASE employees
+
+USE employees
+
+-- department table --
+CREATE TABLE department (
+    --your code here--
+)
+
+CREATE TABLE role (
+    --your code here--
+)
+
+CREATE TABLE employee (
+    --your code here--
+)
